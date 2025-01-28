3b77341b9661e0816eed2e477760b34aa3ac4745
AmiraAdel28 <amira.adel282001@gmail.com>
2025-01-28 22:52:18 +0200

    Q1-Part2

--- Assignment3_CS_Advanced/Program.cs
+++ Assignment3_CS_Advanced/Program.cs
@@ -61,7 +61,7 @@ namespace Assignment3_CS_Advanced
             // -- is divided into small collection called Buckets and Item stored into Buckets
             // -- Prevents repetition by using GetHashCode() and Equality
             // -- Keys must be unique, and null is allowed for values but not keys.
-             
+
             //Dictionary<String , int> NoteBook = new Dictionary<String , int>(new GenericStringEqualityComparer());
             //NoteBook.Add("Eman", 123);
             //NoteBook.Add("Amira", 456);
@@ -103,15 +103,15 @@ namespace Assignment3_CS_Advanced
             // -- in namespace  => System.Collection.Generic
             // -- A generic key-value pair collection with type safety.
 
-            SortedList<String , int> NoteBook = new SortedList<String , int>(new StringSortedDec());
-            NoteBook.Add("Eman", 123);
-            NoteBook.Add("Amira", 456);
-            NoteBook.Add("Khalid", 789);
+            //SortedList<String , int> NoteBook = new SortedList<String , int>(new StringSortedDec());
+            //NoteBook.Add("Eman", 123);
+            //NoteBook.Add("Amira", 456);
+            //NoteBook.Add("Khalid", 789);
 
-            foreach (KeyValuePair<String, int> pair in NoteBook)
-            {
-                Console.WriteLine($"{pair.Key} :: {pair.Value}");
-            }
+            //foreach (KeyValuePair<String, int> pair in NoteBook)
+            //{
+            //    Console.WriteLine($"{pair.Key} :: {pair.Value}");
+            //}
 
 
             #endregion
@@ -123,16 +123,49 @@ namespace Assignment3_CS_Advanced
             // Namespace: System.Collections.Generic
             // Time Complexity => O(1)
 
-            HashSet<Movies> list = new HashSet<Movies>();
-            list.Add(new Movies(101, "Animation", 200));
-            list.Add(new Movies(102, "Harry Poter", 300));
-            list.Add(new Movies(103, "Avatar", 150));
-            list.Add(new Movies(103, "Avatar", 150));
+            //HashSet<Movies> list = new HashSet<Movies>();
+            //list.Add(new Movies(101, "Animation", 200));
+            //list.Add(new Movies(102, "Harry Poter", 300));
+            //list.Add(new Movies(103, "Avatar", 150));
+            //list.Add(new Movies(103, "Avatar", 150));
 
-            foreach(Movies e in list)
+            //foreach(Movies e in list)
+            //{
+            //    Console.WriteLine(e.ToString());
+            //}
+            #endregion
+            #endregion
+
+            #region Part02
+            #region Q1-Part2
+            //Given an array of integers, count the frequency of each element using a hash table.
+
+            int[] Array = { 1, 2, 3, 1, 4, 3, 5, 6, 4, 7, 8 };
+
+            Hashtable Frequency = new Hashtable();
+
+            foreach(int num in Array)
             {
-                Console.WriteLine(e.ToString());
+                if(Frequency.ContainsKey(num))
+                {
+                    Frequency[num] = (int)Frequency[num] + 1;
+                }
+                else
+                {
+                    Frequency[num] = 1;
+                }
             }
+
+            foreach (DictionaryEntry entry in Frequency)
+            {
+                Console.WriteLine($"Element: {entry.Key} ::Frequency : {entry.Value}");
+            }
+            #endregion
+
+            #region
+            #endregion
+
+            #region
             #endregion
             #endregion
         }
