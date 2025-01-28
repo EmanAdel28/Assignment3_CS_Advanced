d33eb9d2dd5f34a013b1b1088ec0b75649e7890d
AmiraAdel28 <amira.adel282001@gmail.com>
2025-01-28 22:12:12 +0200

    Dictionary

--- Assignment3_CS_Advanced/Program.cs
+++ Assignment3_CS_Advanced/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using Assignment3_CS_Advanced.Genaruc_Collection_Dictionary;
 using Assignment3_CS_Advanced.HashTable;
 namespace Assignment3_CS_Advanced
 {
@@ -8,10 +9,11 @@ namespace Assignment3_CS_Advanced
         static void Main(string[] args)
         {
             #region Part01
-            #region 1-Hashtable
             //Write a Report about all Collections Given in the Session and Compare
             //[Structure, Time Complexity, and Business Case With Implemented Examples
             //in Separates Project at the Same Solution]
+
+            #region 1-Hashtable
 
             // 1) Hashtable 
             // -- its store key value pair 
@@ -45,6 +47,30 @@ namespace Assignment3_CS_Advanced
             //{
             //    Console.WriteLine($"{item.Key} :: {item.Value}");
             //}
+            #endregion
+
+            #region 2-Dictionary
+
+            // -- its store key value pair 
+            // --  Backed by a hash table internally.
+            // -- in namespace  => System.Collection.Generic
+            // -- Time Complexity = O(1)
+            // -- A generic key-value pair collection with type safety.
+            // -- is divided into small collection called Buckets and Item stored into Buckets
+            // -- Prevents repetition by using GetHashCode() and Equality
+            // -- Keys must be unique, and null is allowed for values but not keys.
+             
+            Dictionary<String , int> NoteBook = new Dictionary<String , int>(new GenericStringEqualityComparer());
+            NoteBook.Add("Eman", 123);
+            NoteBook.Add("Amira", 456);
+            NoteBook.Add("Khalid", 789);
+            //NoteBook.Add("khalid", 789);
+
+            foreach(KeyValuePair<String , int> pair in NoteBook)
+            {
+                Console.WriteLine($"{pair.Key} :: {pair.Value}");
+            }
+
             #endregion
             #endregion
         }
